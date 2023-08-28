244d85ef95a000f86aecac0c28396e11aa4a99e5
Andrew <andrewnovosel90@gmail.com>
2023-08-28 12:56:53 -0400

    Attempted to customize tailwind bg-color - fail

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,njk}"],
+  content: ["./src/**/*.{html, njk, js}"],
   theme: {
     extend: {
       colors: {
-        primary: "#FFDEA6"
-      }
+        primary: "#A2C1FE",
+      },
     },
   },
   plugins: [],
