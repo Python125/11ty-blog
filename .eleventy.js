02527b02d07c506d9cbd8f8b157ae9cc4b4d1377
Andrew <andrewnovosel90@gmail.com>
2023-08-28 12:23:54 -0400

    Made changes

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
+
   return {
     dir: {
       input: "src",
