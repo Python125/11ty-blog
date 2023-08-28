f6e71c60d43aed44b5be29433fcf9d9ac8937044
Andrew <andrewnovosel90@gmail.com>
2023-08-28 16:34:29 -0400

    Another attempt

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const { DateTime } = require("luxon");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/style.css");
+  eleventyConfig.addPassthroughCopy("./src/input.css");
   eleventyConfig.addPassthroughCopy("tailwind.config.js");
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
